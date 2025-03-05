472ef1f13f9009b4528aae7dd35f3cee5e1c9bb7
stefano <stesatta87@hotmail.it>
2025-03-05 18:09:32 +0100

    installation gtag manager: test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import './../styles/app.scss';
 import {GoogleAnalytics} from "@next/third-parties/google";
+import {Lato} from "next/font/google";
+
+const lato = Lato({
+    subsets: ['latin'],
+    weight: ['300', '400', '700', '900'],
+    display: 'swap'
+})
 
 
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={lato.className}>{children}</body>
             <GoogleAnalytics gaId="G-FVXXJJRYNQ" />
         </html>
     );
