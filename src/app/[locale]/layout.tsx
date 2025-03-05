beb59d46b063b174ac35a396a0943cbf1d14deb6
stefano <stesatta87@hotmail.it>
2025-03-05 18:10:19 +0100

    installation gtag manager: test

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,15 +5,15 @@ import React from "react";
 import NavBar from "@/components/common/navbar/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import {routing} from "@/i18n/routing";
-import { Lato } from 'next/font/google';
+/*import { Lato } from 'next/font/google';*/
 /*import ButtonScrollTop from "@/components/common/button-scroll-to-top/ButtonScrollTop";
 import useScrollToTop from "@/hooks/useScrollToTop";*/
 
-const lato = Lato({
+/*const lato = Lato({
     subsets: ['latin'],
     weight: ['300', '400', '700', '900'],
     display: 'swap'
-})
+})*/
 
 
 export default async function LocaleLayout({children, params}: {
