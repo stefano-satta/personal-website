463c28f96f8649c6dfc37672619448029563496a
stefano <stesatta87@hotmail.it>
2025-12-18 12:44:38 +0100

    fix deploy vercel 500 jsdom

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,12 +1,9 @@
 import useCurrentDate from "@/hooks/useCurrentDate";
 import SocialContacts from "../navbar/SocialContacts";
-import myInfo from "@/utility/my-info";
-import {useTranslations} from "next-intl";
 
 
 const Footer = () => {
     const {getCurrentYear} = useCurrentDate();
-    const t = useTranslations('footer');
 
     return (
         <footer className="footer d-flex flex-column justify-content-center align-items-center py-4 mt-auto bg-dark text-white">
