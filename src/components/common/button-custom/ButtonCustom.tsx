3b51763d96fae8e51939ab8dca8579b7a2c842c1
stefano <stesatta87@hotmail.it>
2026-01-21 19:46:57 +0100

    custom button home page cta added routing to about page

--- src/components/common/button-custom/ButtonCustom.tsx
+++ src/components/common/button-custom/ButtonCustom.tsx
@@ -6,13 +6,17 @@ import icon from "@/utility/icon";
 interface ButtonCustomProps extends HTMLAttributes<HTMLButtonElement> {
     label: string;
     iconType?: Partial<IconProject>;
+    onClickAction?: () => void;
 }
 
 const ButtonCustom = (props: ButtonCustomProps) => {
-    const {label, iconType = icon.arrowUp, className, style} = props;
+    const {label, iconType = icon.arrowUp, className, style, onClickAction} = props;
 
     return (
-        <Button className={`px-5 d-flex rounded-pill bg-white text-black position-relative ${className}`} style={style} id="button-custom">
+        <Button className={`px-5 d-flex rounded-pill bg-white text-black position-relative ${className}`}
+                style={style}
+                id="button-custom"
+                onClick={() => onClickAction?.()}>
             <span className="flex-grow-1">{label}</span>
             <span className="ms-3 p-1 rounded-circle bg-yellow-accent position-absolute z-1">
                 <Icon icon={iconType} className="text-black" style={{transform: 'rotate(45deg)'}}/>
--- src/components/feature/section-home/About.tsx
+++ src/components/feature/section-home/About.tsx
@@ -1,18 +1,25 @@
+"use client"
 import {Col, Container, Row} from "react-bootstrap";
 import {useTranslations} from "next-intl";
 import ButtonCustom from "@/components/common/button-custom/ButtonCustom";
 import AnimatedCounter from "@/components/common/animated-counter/AnimatedCounter";
+import {useRouter} from '@/i18n/routing';
 
 
 const About = () => {
     const t = useTranslations('homepage.whatIDo');
+    const router = useRouter();
+
+    const goToAboutPage = () => {
+        router.push('/about');
+    }
 
     return (
         <section className="bg-black" id="about-section-hero">
             <Container as="div" className="">
                 <p className="text-center text-white fs-4 text-wrap-balance mb-5" dangerouslySetInnerHTML={{__html: t.raw('welcomeMessage')}}></p>
                 <p className="text-center text-white fs-4 text-wrap-balance" dangerouslySetInnerHTML={{__html: t.raw('subMessage')}}></p>
-                <ButtonCustom label={"About me"} className="mx-auto mt-5"/>
+                <ButtonCustom label={"About me"} className="mx-auto mt-5" onClickAction={goToAboutPage}/>
                 <Row className="mt-5 flex-wrap flex-none">
                     <Col xs="12" md="4" className="sm-px-2">
                         <div className="box d-flex flex-column align-items-start justify-content-between text-white">
