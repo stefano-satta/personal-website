297a411e0362119471f5a8559428c393cd1e84bf
stefano <stesatta87@hotmail.it>
2026-01-23 17:38:57 +0100

    fix lottie rendering | WIP

--- src/components/common/lottie-animation/LottieAnimationBlob.tsx
+++ src/components/common/lottie-animation/LottieAnimationBlob.tsx
@@ -1,22 +1,20 @@
 'use client'
-import {useLottie} from "lottie-react";
-import LottieAnimationData from '@/utility/lotties/blob.json';
 import {HTMLAttributes} from "react";
+import {DotLottieReact} from "@lottiefiles/dotlottie-react";
 
 type LottieAnimationBlobProps = HTMLAttributes<HTMLDivElement>;
 
 const LottieAnimationBlob = ({className}: LottieAnimationBlobProps) => {
-    const defaultOptions = {
-        animationData: LottieAnimationData,
-        loop: true,
-    };
-
-    const { View } = useLottie(defaultOptions);
 
     return (
         <div className={className} id="blob-lottie">
-            <div className="w-full">{View}</div>
+            <DotLottieReact
+                src="/lotties/blob.lottie"
+                loop
+                autoplay
+            />
         </div>
+
     )
 }
 
--- src/components/common/lottie-animation/LottieAnimationICountry.tsx
+++ src/components/common/lottie-animation/LottieAnimationICountry.tsx
@@ -1,22 +1,20 @@
 'use client'
-import {useLottie} from "lottie-react";
-import LottieAnimationData from '@/utility/lotties/icountry-lottie.json';
 import {HTMLAttributes} from "react";
+import {DotLottieReact} from "@lottiefiles/dotlottie-react";
 
 type LottieAnimationICountryProps = HTMLAttributes<HTMLDivElement>;
 
 const LottieAnimationICountry = ({className}: LottieAnimationICountryProps) => {
-    const defaultOptions = {
-        animationData: LottieAnimationData,
-        loop: true,
-    };
-
-    const { View } = useLottie(defaultOptions);
 
     return (
         <div className={className} id="icountry-lottie">
-            <div className="w-full">{View}</div>
+            <DotLottieReact
+                src="/lotties/icountry.lottie"
+                loop
+                autoplay
+            />
         </div>
+
     )
 }
 
